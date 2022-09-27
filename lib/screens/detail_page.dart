69121a4769b2b76d758fa5d5fcb955efda4ae0a4
salim-maula <huzaifah956@gmail.com>
2022-09-27 09:31:31 +0700

    finished

--- lib/screens/detail_page.dart
+++ lib/screens/detail_page.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_without_state_management/provider/all_anime.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({Key? key}) : super(key: key);
 
   static const routeName = '/detail-page';
 
-
   @override
   Widget build(BuildContext context) {
+    final animeId = ModalRoute.of(context)?.settings.arguments as String;
 
-  final animeId = ModalRoute.of(context)?.settings.arguments as String;
+    final animeData = Provider.of<Animes>(context).listAnime.firstWhere(
+          (anime_id) => anime_id.animeId == animeId,
+        );
+
     return Scaffold(
-      appBar: AppBar(title: Text('$animeId')),
+      appBar: AppBar(title: Text('Detail')),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+              child: Image.network(animeData.animeImg),
+            ),
+            Text("${animeData.animeTitle}"),
+            Text("${animeData.releasedDate}"),
+          ],
+        ),
+      ),
     );
   }
 }
